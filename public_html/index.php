46d7d1b2273c63f30ffd2a22175a49e40c4883ec
chrisjmccreadie <chris@cjmcsoftware.co.uk>
2011-11-16 02:32:52 +0000

    connect to database

--- public_html/index.php
+++ public_html/index.php
@@ -7,6 +7,18 @@ $database="sellthroughapp";
 mysql_connect("sellthroughapp.mysql.fluxflex.com",$username,$password);
 @mysql_select_db($database) or die( "Unable to select database");
 //close the database 
+$query="SELECT * FROM sellthrough";
+$result=mysql_query($query);
+$num=mysql_numrows($result);
+
+$i=0;
+while ($i < $num) {
+
+echo "yay";
+
+$i++;
+}
+
 mysql_close();
 
 ?>
