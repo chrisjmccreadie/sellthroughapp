4ed1c3ebe2d5b14cbef3bb9667e7407e2cf654da
chrisjmccreadie <chris@cjmcsoftware.co.uk>
2011-11-16 03:14:14 +0000

    added

--- public_html/sellthrough.php
+++ public_html/sellthrough.php
@@ -15,10 +15,12 @@ $num=mysql_numrows($result);
 
 $i=0;
 echo "<table>";
-echo "<tr><td>Style</td><td>Number Sold</td></tr>";
+echo "<tr><td>Style</td><td>Fabric</td><td>Desc</td><td>Colour</td><td>WSP</td><td>Number Sold</td></tr>";
 while ($i < $num) {
     echo "<tr>";
-    echo "<td>".mysql_result($result,$i,"style")."</td><td><input type='text' name='num$i' /></td>";
+    echo "<td>".mysql_result($result,$i,"name")."</td><td>$complete</td>";
+    
+    echo "<td>".mysql_result($result,$i,"style")."</td><td>".mysql_result($result,$i,"fabric")."</td><td>".mysql_result($result,$i,"desc")."</td><td>".mysql_result($result,$i,"colour")."</td><td>".mysql_result($result,$i,"wholesaleprice")."</td><td><input type='text' name='num$i' /></td>";
     echo "</tr>";
     $i++;
 }
