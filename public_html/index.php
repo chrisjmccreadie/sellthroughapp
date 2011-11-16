6e5441471013f32de287b5d61eead541a96f62f5
chrisjmccreadie <chris@cjmcsoftware.co.uk>
2011-11-16 02:30:38 +0000

    connect to database

--- public_html/index.php
+++ public_html/index.php
@@ -1,3 +1,12 @@
 <?php
-echo "eee";
+//do it
+//fecth a list of urls mysql_connect(localhost,$username,$password);
+$username="sellthroughapp";
+$password="SlTbckNB";
+$database="sellthroughapp";
+mysql_connect("sellthroughapp.mysql.fluxflex.com",$username,$password);
+@mysql_select_db($database) or die( "Unable to select database");
+//close the database 
+mysql_close();
+
 ?>
