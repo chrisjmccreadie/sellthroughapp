c80ac7d529eb6c4d7c870ccaafd7c2d665212a82
chrisjmccreadie <chris@cjmcsoftware.co.uk>
2011-11-21 00:40:56 +0000

    test

--- public_html/genrandom.php
+++ public_html/genrandom.php
@@ -15,7 +15,7 @@ while ($i < $num) {
     if (!in_array($n, $rn)) {
         $rn[] = $n;
         //update the database
-        $query="update  `sellthrough` set rcode ='$n' where name = '".mysql_result($result,$i,"name")."'";
+        $query2="update  `sellthrough` set rcode ='$n' where name = '".mysql_result($result,$i,"name")."'";
         $result2=mysql_query($query2);
         echo mysql_error();
         $i++;
