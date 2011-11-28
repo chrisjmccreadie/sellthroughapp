83a251dd1267fec478e311f6d8e653ec64be101a
chrisjmccreadie <chris@cjmcsoftware.co.uk>
2011-11-28 04:14:46 +0000

    jquery

--- public_html/updateit.php
+++ public_html/updateit.php
@@ -6,12 +6,12 @@ $password="SlTbckNB";
 $database="sellthroughapp";
 mysql_connect("sellthroughapp.mysql.fluxflex.com",$username,$password);
 @mysql_select_db($database) or die( "Unable to select database");
-print_r($_POST);
+//print_r($_POST);
 foreach ($_POST as $k => $v) {
     $query="INSERT INTO `sellthroughapp`.`sale` (`id`, `sellid`, `quantity`, `dateadded`,`rcode`) VALUES (NULL, '$k', '$v', CURRENT_TIMESTAMP,'$rcode')";
     $result=mysql_query($query);
     //update they have been added
 }
-
+echo "Thank you";
 
 ?>
