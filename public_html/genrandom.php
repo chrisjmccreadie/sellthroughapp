0df5e7fac23023c5cb8adb77fe2daec5ec0eefb1
chrisjmccreadie <chris@cjmcsoftware.co.uk>
2011-11-28 01:30:40 +0000

    set random code

--- public_html/genrandom.php
+++ public_html/genrandom.php
@@ -7,7 +7,7 @@ $database="sellthroughapp";
 mysql_connect("sellthroughapp.mysql.fluxflex.com",$username,$password);
 @mysql_select_db($database) or die( "Unable to select database");
 //close the database 
-$query="SELECT * FROM `sellthrough`  where rcode = 0 group by  name ";
+$query="SELECT * FROM `sellthrough2`  where rcode = 0 group by  name ";
 $result=mysql_query($query);
 echo mysql_error();
 $num=mysql_numrows($result);
@@ -15,11 +15,12 @@ while ($i < $num) {
     $n = rand(0, 10000000);
     if (!in_array($n, $rn)) {
         $rn[] = $n;
+        $url = "http://sellthroughapp.fluxflex.com/sellthrough.php?rcode="+$n;
         //update the database
         $name = mysql_result($result,$i,"name");
         $name = str_replace("'","''",$name);
         echo $name."</br>";
-        $query2="update  `sellthrough` set rcode ='$n' where name = '".$name."'";
+        $query2="update  `sellthrough` set rcode ='$n',url='$url' where name = '".$name."'";
         $result2=mysql_query($query2);
         echo mysql_error();
         $i++;
