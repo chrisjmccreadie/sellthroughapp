9e4f3a5631541bfc828fba651c456ffcf9b8b261
chrisjmccreadie <chris@cjmcsoftware.co.uk>
2011-11-28 04:13:40 +0000

    jquery

--- public_html/updateit.php
+++ public_html/updateit.php
@@ -1,19 +1,17 @@
 <?php
-print_r($_POST);
-foreach ($_POST as $k => $v) {
-    echo "$k : $v\n";
-}
-exit;
 //fecth a list of urls mysql_connect(localhost,$username,$password);
+$rcode = rand(0,11111111);
 $username="sellthroughapp";
 $password="SlTbckNB";
 $database="sellthroughapp";
 mysql_connect("sellthroughapp.mysql.fluxflex.com",$username,$password);
 @mysql_select_db($database) or die( "Unable to select database");
-//close the database 
-$customer = $_GET["rcode"];
-//echo $customer;
-//$query="SELECT * FROM `sellthrough2` where rcode = '$customer'  group by style, name  order by name ";
-//$result=mysql_query($query);
-echo "done";
+print_r($_POST);
+foreach ($_POST as $k => $v) {
+    $query="INSERT INTO `sellthroughapp`.`sale` (`id`, `sellid`, `quantity`, `dateadded`,`rcode`) VALUES (NULL, '$k', '$v', CURRENT_TIMESTAMP,'$rcode')";
+    $result=mysql_query($query);
+    //update they have been added
+}
+
+
 ?>
