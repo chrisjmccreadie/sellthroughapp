86803ae77894a95115befd105d63a0d6b64d4fc3
chrisjmccreadie <chris@cjmcsoftware.co.uk>
2011-12-09 02:57:34 +0000

    udd

--- public_html/stylebreakdown.php
+++ public_html/stylebreakdown.php
@@ -7,15 +7,22 @@ $database="sellthroughapp";
 mysql_connect("sellthroughapp.mysql.fluxflex.com",$username,$password);
 @mysql_select_db($database) or die( "Unable to select database");
 //close the database 
-$query="SELECT * FROM `sellthrough2` group by style order by style";
+$query="SELECT style, country, COUNT( country ) AS ccount
+FROM  `sellthrough2` 
+GROUP BY style";
 $result=mysql_query($query);
 $num=mysql_numrows($result);
 $i = 0;
 while ($i < $num)
 {
     echo mysql_result($result,$i,"style");
+     echo mysql_result($result,$i,"country");
+      echo mysql_result($result,$i,"count");
+    echo "<br/>";
     $i++;
+//count
 }
+
 
 
 
