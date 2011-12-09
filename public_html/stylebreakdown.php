2ec64a4fb2d8dfdcf9e76eaccf5d4123ce3ae8af
chrisjmccreadie <chris@cjmcsoftware.co.uk>
2011-12-09 03:02:03 +0000

    udd

--- public_html/stylebreakdown.php
+++ public_html/stylebreakdown.php
@@ -13,15 +13,19 @@ GROUP BY style";
 $result=mysql_query($query);
 $num=mysql_numrows($result);
 $i = 0;
+$out =  "<table>";
+$out = $out. "<tr><td>Style</td><td>Country<td/><td>Count</td></tr>";
 while ($i < $num)
 {
-    echo mysql_result($result,$i,"style");
-     echo mysql_result($result,$i,"country");
-      echo mysql_result($result,$i,"count");
-    echo "<br/>";
+    $out = $out. "<tr><td>";
+    $out = $out. mysql_result($result,$i,"style")."</td>";
+    $out = $out."<td>".mysql_result($result,$i,"country")."</td>";
+    $out = $out."<td>". mysql_result($result,$i,"ccount")."</td>";
+    $out = $out. "<td></tr>";
     $i++;
 //count
 }
+
 
 
 
