2c1e7b600b1c106c756b02f579f20d61c5824a3b
chrisjmccreadie <chris@cjmcsoftware.co.uk>
2011-12-09 01:58:31 +0000

    update

---
+++ public_html/stylebreakdown.php
@@ -0,0 +1,20 @@
+<?php
+//do it
+//fecth a list of urls mysql_connect(localhost,$username,$password);
+$username="sellthroughapp";
+$password="SlTbckNB";
+$database="sellthroughapp";
+mysql_connect("sellthroughapp.mysql.fluxflex.com",$username,$password);
+@mysql_select_db($database) or die( "Unable to select database");
+//close the database 
+$query="SELECT * FROM `sellthrough2` group by style order by style";
+$result=mysql_query($query);
+$num=mysql_numrows($result);
+$i = 0;
+while ($i < $num)
+    echo mysql_result($result,$i,"style");
+    $i++;
+}
+
+
+?>
