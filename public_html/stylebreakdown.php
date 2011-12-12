a862593dbccb2d2a48324b9eb8192deadea37c0e
chrisjmccreadie <chris@cjmcsoftware.co.uk>
2011-12-12 03:16:33 +0000

    tester

--- public_html/stylebreakdown.php
+++ public_html/stylebreakdown.php
@@ -1,62 +1,4 @@
-<?php
-//do it
-//fecth a list of urls mysql_connect(localhost,$username,$password);
-$username="sellthroughapp";
-$password="SlTbckNB";
-$database="sellthroughapp";
-mysql_connect("sellthroughapp.mysql.fluxflex.com",$username,$password);
-@mysql_select_db($database) or die( "Unable to select database");
-//close the database 
-
-
-$query = "select country from sellthrough2 group by country";
-$result=mysql_query($query);
-$num=mysql_numrows($result);
-
-$out =  "<table><tr>";
-
-$country = array();
-$i = 0;
-while ($i < $num)
-{
-  //  echo  mysql_result($result,$i,"country")."</br>";
-        $country[$i]["name"] = mysql_result($result,$i,"country");
-        $country[$i]["count"] = 0;
-
-        $out = $out."<th>".mysql_result($result,$i,"country")."</th>";
-        $out = $out."<th>%</th>";
-    $i++;   
-}
-//echo "wah ";
-//print_r($country);
-
-
-$query="SELECT style, country, COUNT( country) AS ccount
-FROM  `sellthrough2` 
-GROUP BY country,style
-ORDER BY style,country
-";
-
-$result=mysql_query($query);
-$num=mysql_numrows($result);
-echo "$num";
-$i = 0;
-$style = "";
-while ($i < $num)
-{
-    if ($style !=  mysql_result($result,$i,"style"))
-    {
-        if ($style != "")
-        {
-            //reset the vars
-            $out = $out."<tr><td>$style</td>";
-            foreach($country as $item )
-            {
-                $out = $out."<td>"$item["count"]."</td>";
-                $item["count"] = 0;   
-            }
-            $out = $out."</tr>";
-            
+       
         }
         //copy the style 
         $style = mysql_result($result,$i,"style")            
@@ -67,28 +9,6 @@ while ($i < $num)
 exit;
 
 
-//new query
-$query="SELECT style, country, COUNT( country) AS ccount
-FROM  `sellthrough2` 
-GROUP BY country,style
-ORDER BY style,country
-";
-$result=mysql_query($query);
-$num=mysql_numrows($result);
-$i = 0;
-$out = $out. "<tr><td>Style</td><td>Country<td/><td>Count</td></tr>";
-while ($i < $num)
-{
-    
-    $out = $out. "<tr><td>";
-    $out = $out. mysql_result($result,$i,"style")."</td>";
-    $out = $out."<td>".mysql_result($result,$i,"country")."</td>";
-    $out = $out."<td>". mysql_result($result,$i,"ccount")."</td>";
-    $out = $out. "<td></tr>";
-    $i++;
-//count
-}
-echo $out."</table>";
 
 
 
