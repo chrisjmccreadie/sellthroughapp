864d1e4455d56fc9ee54508bef0f03231d370b55
chrisjmccreadie <chris@cjmcsoftware.co.uk>
2011-12-12 06:08:10 +0000

    edit customers sold

---
+++ public_html/updatecustomersold.php
@@ -0,0 +1,23 @@
+<?php
+//fecth a list of urls mysql_connect(localhost,$username,$password);
+//echo "kk".$_GET['data'];
+
+$username="sellthroughapp";
+$password="SlTbckNB";
+$database="sellthroughapp";
+mysql_connect("sellthroughapp.mysql.fluxflex.com",$username,$password);
+@mysql_select_db($database) or die( "Unable to select database");
+//print_r($_POST);
+foreach ($_POST as $k => $v) {
+    echo "$k : $v";
+    $query="update `sellthroughapp`.`sellthrough2` set `quantity` ='$v' where id = '$k'";
+    $result=mysql_query($query);
+    echo mysql_error();
+
+    //update they have been added
+}
+    header('Location: index.php');
+
+echo "Thank you";
+
+?>
