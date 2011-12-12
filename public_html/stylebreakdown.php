f9f298486d5ec86b39119d40e22c66187781a792
chrisjmccreadie <chris@cjmcsoftware.co.uk>
2011-12-12 02:20:57 +0000

    fff

--- public_html/stylebreakdown.php
+++ public_html/stylebreakdown.php
@@ -41,15 +41,7 @@ $num=mysql_numrows($result);
 $i = 0;
 while ($i < $num)
 {
-    //loop till we find the country
-    foreach($country as $item )
-    {
-        if (mysql_result($result,$i,"country") == $item["country"])
-        {
-        
-             echo "Style :".mysql_result($result,$i,"style")."Country :".$item["name"]." Count :".$item["count"]."</br>";
-        }
-    }
+    
     $i++;
 }
 exit;
