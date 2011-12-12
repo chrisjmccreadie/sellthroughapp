b61902777415bddffb9c9f9d2a4a02756ec3b2c6
chrisjmccreadie <chris@cjmcsoftware.co.uk>
2011-12-12 05:08:07 +0000

    added edit customer

---
+++ public_html/editcustomer.php
@@ -0,0 +1,73 @@
+<html>
+<script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js?ver=3.1.2'></script>
+<script type="text/javascript">
+function updatesell()
+{
+    dataString  = '{';
+    i=0;
+     $(".txt").each(function() {
+ 
+            //add only if the value is number
+            if(!isNaN(this.value) && this.value.length!=0) {
+                //alert(this.value+' : '+this.id);
+                if(i == 0)
+                    dataString = dataString+this.value+':'+this.id;
+                else
+                    dataString = dataString+','+this.value+':'+this.id;
+
+        i++;
+        }
+ 
+        }); 
+    dataString  = dataString+'}';
+
+alert(dataString);
+        
+
+
+$.get("update.php?data=", dataString,
+   function(data){
+     alert("Data Loaded: " + data);
+   });
+
+     
+        return false;
+}
+</script>
+<body>
+<form name="form1" id="form1" action="updateit.php" method="POST">
+<?php
+//do it
+//fecth a list of urls mysql_connect(localhost,$username,$password);
+$username="sellthroughapp";
+$password="SlTbckNB";
+$database="sellthroughapp";
+mysql_connect("sellthroughapp.mysql.fluxflex.com",$username,$password);
+@mysql_select_db($database) or die( "Unable to select database");
+//close the database 
+$customer = $_GET["id"];
+//echo $customer;
+$query="SELECT * FROM `sellthrough2` where id = '$customer'  order by style";
+$result=mysql_query($query);
+$num=mysql_numrows($result);
+
+$i=0;
+echo "<table>";
+echo "<tr><td>Style</td><td>Quantity</td><td>Colour</td><td>Price</td><td>Number Bought</td></tr>";
+while ($i < $num) {
+    if (mysql_result($result,$i,"quantity") != '0')
+    {
+        $id = mysql_result($result,$i,"id");
+        echo "<tr>";
+        echo "<td>".mysql_result($result,$i,"style")."</td><td>".mysql_result($result,$i,"quantity")."</td><td>".mysql_result($result,$i,"colour")."</td><td>".mysql_result($result,$i,"price")."</td><td><input type='text' name='$id' id='$id' class='txt' /></td>";
+        echo "</tr>";
+    }
+    $i++;
+}
+echo "</table>";
+echo '<input type="submit" value="Submit"/>';
+mysql_close();
+?>
+</form>
+</body>
+</html>
