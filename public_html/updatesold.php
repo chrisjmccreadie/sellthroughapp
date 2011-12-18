77b9636a229add6892ae3979c6fb1157d02e3101
chrisjmccreadie <chris@cjmcsoftware.co.uk>
2011-12-18 23:33:43 +0000

    num sold

--- public_html/updatesold.php
+++ public_html/updatesold.php
@@ -16,6 +16,9 @@ while ($i < $num) {
         $sold = mysql_result($result,$i,"quantity");
        
        $query2="SELECT * FROM `sellthrough2` where id = '$sellid' ";
+       $result2=mysql_query($query2);
+       $sold2 = mysql_result($result2,0,"quantity");
+      
 `      $sold2 = 2;
        echo "$sellid : $sold : $sold2</br>";
        
