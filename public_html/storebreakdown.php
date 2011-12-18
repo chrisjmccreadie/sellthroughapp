d95a7f419e0eaea99bcb0a5b9a8f84c4f2f8dc5e
chrisjmccreadie <chris@cjmcsoftware.co.uk>
2011-12-18 23:11:26 +0000

    sb5

--- public_html/storebreakdown.php
+++ public_html/storebreakdown.php
@@ -39,11 +39,15 @@ while ($i < $num)
       $query2 = "SELECT name,sum(quantity) as bought FROM `sellthrough2` WHERE name = '$name'";
     $result2=mysql_query($query2);
     $bought = mysql_result($result2,0,"bought");
+  
+     $sold = mysql_result($result2,0,"sold");
+  if ($sold == 0)
+    {
  
-      $query2 = "SELECT name,sum(sold) as sold FROM `sellthrough2` WHERE name = '$name'";
-    $result2=mysql_query($query2);
-    $sold= mysql_result($result2,0,"sold");
-    
+        $query2 = "SELECT name,sum(sold) as sold FROM `sellthrough2` WHERE name = '$name'";
+        $result2=mysql_query($query2);
+        $sold= mysql_result($result2,0,"sold");
+    }
      //echo "$per ff";
      $per = $sold / $bought * 100;
      $out = $out."<tr class=\"odd gradeX\"><td>$name</td><td>$bought</td><td>$sold</td><td>$per %</td></tr>";
