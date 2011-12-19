64b53f5f9ff361cfe278da2d90118eeb1be69fdc
chrisjmccreadie <chris@cjmcsoftware.co.uk>
2011-12-19 00:16:10 +0000

    jfhdjfh

--- public_html/updatesold.php
+++ public_html/updatesold.php
@@ -7,6 +7,12 @@ $database="sellthroughapp";
 mysql_connect("sellthroughapp.mysql.fluxflex.com",$username,$password);
 @mysql_select_db($database) or die( "Unable to select database");
 //close the database 
+
+
+//clear the rsold
+$query ="UPDATE  `sellthroughapp`.`sellthrough2` SET  `rsold` =  '0'"
+$result=mysql_query($query);
+
 $query="SELECT * FROM `sale`  order by sellid";
 $result=mysql_query($query);
 $num=mysql_numrows($result);
@@ -19,7 +25,11 @@ while ($i < $num)
        $result2=mysql_query($query2);
        $sold2 = mysql_result($result2,0,"quantity");
        //$sold2 = 2;
-       echo "$sellid : $sold : $sold2</br>";
-       $i++;   
+      // echo "$sellid : $sold : $sold2</br>";
+      $sold2 = $sold+$sold2;
+      $query3 ="UPDATE  `sellthroughapp`.`sellthrough2` SET  `rsold` =  '$sold2' where id = '$sellid'"
+$result3=mysql_query($query3);
+
+      $i++;   
 }
 ?>
