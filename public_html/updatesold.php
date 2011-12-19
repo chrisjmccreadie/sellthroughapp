9f733f8b0a112438c63548246a380f314ce82007
chrisjmccreadie <chris@cjmcsoftware.co.uk>
2011-12-19 00:08:58 +0000

    jfhdjfh

--- public_html/updatesold.php
+++ public_html/updatesold.php
@@ -11,22 +11,15 @@ $query="SELECT * FROM `sale`  order by sellid";
 $result=mysql_query($query);
 $num=mysql_numrows($result);
 $i=0;
-while ($i < $num) {
-       // $sellid = mysql_result($result,$i,"sellid");
-        //$sold = mysql_result($result,$i,"quantity");
-       
-     //  git  $query2="SELECT * FROM `sellthrough2` where id = '$sellid' ";
-      // $result2=mysql_query($query2);
-     //  $sold2 = mysql_result($result2,0,"quantity");
-     
-`      
-
-//$sold2 = 2;
-  //     echo "$sellid : $sold : $sold2</br>";
-       
- 
- $i++;   
+while ($i < $num)
+{
+       //$sellid = mysql_result($result,$i,"sellid");
+       //$sold = mysql_result($result,$i,"quantity");
+       //git  $query2="SELECT * FROM `sellthrough2` where id = '$sellid' ";
+       //$result2=mysql_query($query2);
+       //$sold2 = mysql_result($result2,0,"quantity");
+       //$sold2 = 2;
+       //echo "$sellid : $sold : $sold2</br>";
+       $i++;   
 }
-
-
 ?>
