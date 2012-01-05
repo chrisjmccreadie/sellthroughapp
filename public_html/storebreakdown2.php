6fd47b1e31057a30be042a6f4de4665f33d77036
chrisjmccreadie <chris@cjmcsoftware.co.uk>
2012-01-05 13:50:24 +0000

    sbd2

--- public_html/storebreakdown2.php
+++ public_html/storebreakdown2.php
@@ -16,7 +16,19 @@ $query="SELECT * FROM `sellthrough2` where name = '$customer'  order by style";
 $result=mysql_query($query);
 $num=mysql_numrows($result);
 $i=0;
-
+echo "<table>";
+echo "<tr><td>Style</td><td>Colour</td><td>Size</td><td>Price</td><td>Number Sold</td><td>Number Bought</td></tr>";
+while ($i < $num) {
+    if (mysql_result($result,$i,"quantity") != '0')
+    {
+        $id = mysql_result($result,$i,"id");
+        echo "<tr>";
+       // echo "<td>".mysql_result($result,$i,"style")."</td><td>".mysql_result($result,$i,"colour")."</td><td>".mysql_result($result,$i,"size")."</td><td>".mysql_result($result,$i,"price")."</td><td>".mysql_result($result,$i,"sold")".</td>";
+        echo "</tr>";
+    }
+    $i++;
+}
+echo "</table>";
 echo '<input type="submit" value="Submit"/>';
 mysql_close();
 ?>
