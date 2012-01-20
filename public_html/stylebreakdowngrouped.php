13ae620bbe14e2e9dce739969d5b644245ad7da3
chrisjmccreadie <chris@cjmcsoftware.co.uk>
2012-01-20 18:15:44 +0000

    new breakdown

--- public_html/stylebreakdowngrouped.php
+++ public_html/stylebreakdowngrouped.php
@@ -30,36 +30,18 @@ $query = "select country from sellthrough2 group by country";
 $result=mysql_query($query);
 $num=mysql_numrows($result);
 $out =  '<table cellpadding="0" cellspacing="0" border="0" class="display" id="example"><thead>';
-$country = array();
 $i = 0;
 $out = $out."<tr><th>Name</th>";
 while ($i < $num)
 {
   //  echo  mysql_result($result,$i,"country")."</br>";
-        $country[$i]["name"] = mysql_result($result,$i,"country");
-        $country[$i]["count"] = 0;
+      
         $out = $out."<th>".mysql_result($result,$i,"country")."</th>";
         $out = $out."<th>%</th>";
     $i++;   
 }
 $out = $out."<th>Total</th></tr></thead><tbody>";
-//echo "wah ";
-//print_r($country);
 
-
-$query="SELECT * from style";
-$result=mysql_query($query);
-$num=mysql_numrows($result);
-echo mysql_error();
-$i = 0;
-while ($i <= $num)
-{
-    $name=  mysql_result($result,$i,"name");
-    $styleid=  mysql_result($result,$i,"id");
-   echo  $styleid;
-    
-    $i++;
-}
 echo $out."</tbody></table>";
 
 ?>
