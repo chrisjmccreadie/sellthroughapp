8eeafb4130858882cc63dd7e056acae14343a56c
chrisjmccreadie <chris@cjmcsoftware.co.uk>
2012-01-20 18:21:14 +0000

    new breakdown

--- public_html/stylebreakdowngrouped.php
+++ public_html/stylebreakdowngrouped.php
@@ -47,10 +47,24 @@ $num=mysql_numrows($result);
 echo mysql_error();
 $i = 0;
 
-while ($i <= $num)
+while ($i < $num)
 {
     $name=  mysql_result($result,$i,"name");
     $styleid=  mysql_result($result,$i,"id");
+    
+    //get the countries
+    $query2="SELECT style, country, count(country) as ccount from sellthrough2 where styleid='$styleid' group by country";
+    $result2=mysql_query($query2);
+    $num2=mysql_numrows($result2);
+    echo mysql_error();
+    $i2 = 0;
+    while ($i2 < $num2)
+    {
+        $country = mysql_result($result2,$i2,"country");
+         $ccount = mysql_result($result2,$i2,"cccount");
+         echo "$name : $country : $ccount";
+         $i2++;
+    }
     $i = $i+1;
 }
 
