e79122adcb33485c881f7ebaed000bf17f7d3769
chrisjmccreadie <chris@cjmcsoftware.co.uk>
2012-01-20 18:12:13 +0000

    new breakdown

--- public_html/stylebreakdowngrouped.php
+++ public_html/stylebreakdowngrouped.php
@@ -29,9 +29,7 @@ mysql_connect("sellthroughapp.mysql.fluxflex.com",$username,$password);
 $query = "select country from sellthrough2 group by country";
 $result=mysql_query($query);
 $num=mysql_numrows($result);
-
 $out =  '<table cellpadding="0" cellspacing="0" border="0" class="display" id="example"><thead>';
-
 $country = array();
 $i = 0;
 $out = $out."<tr><th>Name</th>";
@@ -58,20 +56,7 @@ while ($i <= $num)
 {
     $name=  mysql_result($result,$i,"name");
     $styleid=  mysql_result($result,$i,"id");
-   
-    //get the countries
-    $query2="SELECT style, country, count(country) as ccount from sellthrough2 where styleid='$styleid' group by country";
-    $result2=mysql_query($query2);
-    $num2=mysql_numrows($result2);
-    echo mysql_error();
-    $i2 = 0;
-    while ($i2 <= $num2)
-    {
-        $country = mysql_result($result2,$i2,"country");
-         $ccount = mysql_result($result2,$i2,"cccount");
-         echo "$name : $country : $ccount";
-         $i2++;
-    }
+  
 $i ++;
 }
 echo $out."</tbody></table>";
